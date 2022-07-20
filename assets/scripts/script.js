f5a6ebef8bc72aa412ea01f587fa9931aaa3c4de
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2022-07-20 13:52:59 +0430

    fixing carousel

---
+++ assets/scripts/script.js
@@ -0,0 +1,28 @@
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     autoplay: true,
+//     autoplayTimeout: 500,
+//     autoplayHoverPause: false,
+//     margin: 10,
+//     responsiveClass: true,
+//     responsive: {
+//         0: {
+//             items: 1,
+//             nav: true
+//         },
+//         600: {
+//             items: 2,
+//             nav: false
+//         },
+//         1000: {
+//             items: 3,
+//             nav: true,
+//             loop: false
+//         }
+//     }
+// })
+
+$('#myCarousel').carousel({
+    interval: 3000,
+    cycle: true
+});

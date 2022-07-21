55bff7e72128969d00103cac8bc5ddbd18305252
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2022-07-21 13:58:16 +0430

    Animate Hamburger Menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -22,7 +22,19 @@
 //     }
 // })
 
-$('#myCarousel').carousel({
-    interval: 3000,
-    cycle: true
+// $('#myCarousel').carousel({
+//     interval: 3000,
+//     cycle: true
+// });
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
 });

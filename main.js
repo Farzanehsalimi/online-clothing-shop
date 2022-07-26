1f4e29657fae58f0afde7a2bf0edf75d1c608738
Farzaneh Salimi <farzanehsalimi16@gmail.com>
2022-07-26 23:04:54 +0430

    carousel

---
+++ main.js
@@ -0,0 +1,39 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel();
+});
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 18,
+    stagePadding: 50,
+    autoplay: true,
+    autoplayTimeout: 1200,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            stagePadding: 0,
+
+        },
+        768: {}
+    }
+});
+// $('.play').on('click', function() {
+//     owl.trigger('play.owl.autoplay', [1000])
+// })
+// $('.stop').on('click', function() {
+//     owl.trigger('stop.owl.autoplay')
+// })
